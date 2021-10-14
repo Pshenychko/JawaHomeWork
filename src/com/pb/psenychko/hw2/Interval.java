e835b22c19b15e2e3f91751aa50dba79ea22a551
Pshenychko Oleksandr <aleksandr.pshenichko@privatbank.ua>
2021-10-15 00:42:03 +0300

    init commit

--- src/com/pb/psenychko/hw2/Interval.java
+++ src/com/pb/psenychko/hw2/Interval.java
@@ -1,5 +1,21 @@
 package com.pb.psenychko.hw2;
 
-public class Interval {
+import java.util.Scanner;
 
+public class Interval {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter digit between 0 and 100");
+        int a = scan.nextInt();
+
+        if (a<0 || a>100) { System.out.println("no no no"); } else {
+        if (a<=14) {System.out.println("0-14");} else {
+            if(a<=35) {System.out.println("15-35");} else {
+                if(a<=50) {System.out.println("36-50");} else {
+                    System.out.println("51-100");
+                }
+        }
+        }
+    }
+    }
 }
