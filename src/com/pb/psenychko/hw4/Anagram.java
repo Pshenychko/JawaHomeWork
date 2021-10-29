9bf9a16c192f8fea412cb92c545e766cc220313c
Pshenychko Oleksandr <aleksandr.pshenichko@privatbank.ua>
2021-10-29 23:04:25 +0300

    init commit

--- src/com/pb/psenychko/hw4/Anagram.java
+++ src/com/pb/psenychko/hw4/Anagram.java
@@ -7,16 +7,36 @@ import java.util.Scanner;
 public class Anagram {
     static String EnterString() {
         Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
-        return str;
+        return scan.nextLine();
     }
 
     static boolean isAnagram(String str1, String str2) {
         char[] s1 = str1.toCharArray();
         char[] s2 = str2.toCharArray();
-        Arrays.sort(s1);
-        Arrays.sort(s2);
-        return Arrays.equals(s2,s1);
+        StringBuilder An1 = new StringBuilder();
+        StringBuilder An2 = new StringBuilder();
+
+        for(int i = 0; i < s1.length; i ++) {
+            if(Character.isLetter(s1[i])) {
+                String x;
+                x = Character.toString(s1[i]);
+                An1.append(x);
+            }
+        }
+
+        for(int i = 0; i < s2.length; i ++) {
+            if(Character.isLetter(s2[i])) {
+                String x;
+                x = Character.toString(s2[i]);
+                An2.append(x);
+            }
+        }
+        char[] an1 = An1.toString().toCharArray();
+        char[] an2 = An2.toString().toCharArray();
+
+        Arrays.sort(an1);
+        Arrays.sort(an2);
+        return Arrays.equals(an1,an2);
     }
 
     public static void main(String[] args) {
